6bbc25886c0f5741ed3a8b71f81521f50bf320f2
huyang <huwsun@gmail.com>
2018-01-11 21:36:08 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,31 @@
-// type Atom = (String, usize);
-// type Molecule = Vec<Atom>;
-fn foo<'a>(x: &'a str,y:&'a str) ->&'a str {
-    if 9%2==0 {x} else {y}
-}
+extern crate atom;
 
-fn check(s:&str) ->&str
-// where 'a:'b
- {
-    //println!("{}",s);
-    return s;
-}
+use atom::*;
+use std::io::Write;
 
 fn main() {
-    let x="hi";
- let e;
- {
-    let y="you";
-    e=y;
-    //e=check(y);
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 5 {
+        writeln!(std::io::stderr(),
+                 "Usage: mandelbrot FILE PIXELS UPPERLEFT LOWERRIGHT")
+            .unwrap();
+        writeln!(std::io::stderr(),
+                 "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
+                 args[0])
+            .unwrap();
+        std::process::exit(1);
+    }
+    let bounds = parse_pair(&args[2], 'x').expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3]).expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+    //render(&mut pixels, bounds, upper_left, lower_right);
 
-    println!("{:p}",y );
- }
-    println!("Hello, world! {:p}",e);
+    do_parallel_render(&mut pixels, bounds, upper_left, lower_right);
+
+
+    write_image(&args[1], &pixels, bounds)
+        .expect("error writing PNG file");
 }
+
+
